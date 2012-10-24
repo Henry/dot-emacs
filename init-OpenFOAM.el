2c51382e0ac77664902a2290826401a22192135d
Henry <Henry>
2012-10-24 19:43:41 +0100

    Updated clang support

--- init-OpenFOAM.el
+++ init-OpenFOAM.el
@@ -569,12 +569,6 @@
              (expand-file-name "~/.emacs.d/packages/emacs-clang-complete-async"))
 
 (require 'auto-complete-config)
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/auto-complete-clang"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/emacs-clang-complete-async"))
-
-(require 'auto-complete-config)
 (require 'auto-complete-clang-async)
 
 (defun ac-cc-mode-setup ()
