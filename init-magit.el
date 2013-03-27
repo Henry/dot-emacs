bd85bd58505aad801972f3f7358d0d556437c51a
Henry <hweller0@gmail.com>
2013-03-27 17:41:15 +0000

    Use magit-blame rather than mo-git-blame

--- init-magit.el
+++ init-magit.el
@@ -6,12 +6,9 @@
              (expand-file-name "~/.emacs.d/packages/magit") t)
 
 (require 'magit)
+(require 'magit-blame)
 
 (define-key my-map "g" 'magit-status)
-
-(add-to-list 'load-path (expand-file-name  "~/.emacs.d/packages/mo-git-blame"))
-(load "mo-git-blame")
-(load "git-blame")
 
 ;; -----------------------------------------------------------------------------
 ;;; init-magit.el ends here
