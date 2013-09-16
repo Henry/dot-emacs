2b50118fc4f12bc9387ed053330db6e39d4d3669
Henry <Henry>
2013-09-16 09:55:29 +0100

    Add support for dirtree

--- init-e2wm.el
+++ init-e2wm.el
@@ -1,5 +1,13 @@
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/emacs-window-manager"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/emacs-window-layout"))
+(add-to-list
+ 'load-path (expand-file-name "~/.emacs.d/packages/emacs-window-manager"))
+(add-to-list
+ 'load-path (expand-file-name "~/.emacs.d/packages/emacs-window-layout"))
 
 (require 'e2wm)
+(require 'windata)
+;(require 'tree-mode)
+;(require 'dirtree)
+(autoload 'dirtree "tree-mode" "tree-mode" t)
+(autoload 'dirtree "dirtree" "dirtree" t)
+
 (global-set-key (kbd "M-+") 'e2wm:start-management)
