0173bc4c9c24b425d81583088e8446ada3df7160
Henry <hweller0@gmail.com>
2014-03-24 10:21:10 +0000

    Updated

--- init-eemacs.el
+++ init-eemacs.el
@@ -5,29 +5,41 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/my-lisp/tree-mode"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/paredit"))
 (add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/haskell-mode"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/icicles"))
+(add-to-list 'load-path
              (expand-file-name "~/.emacs.d/packages/auctex"))
 (add-to-list 'load-path
              (expand-file-name "~/.emacs.d/packages/ebib/src"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/bbdb/lisp"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/completion-ui"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/icicles"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/haskell-mode"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/gnuplot"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/emms/lisp"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/emacs-calfw"))
 (add-to-list 'load-path (expand-file-name  "~/.emacs.d/packages/git-modes"))
 (add-to-list 'load-path (expand-file-name  "~/.emacs.d/packages/magit"))
 (add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/yasnippet"))
+(add-to-list 'load-path
              (expand-file-name "~/.emacs.d/packages/imaxima"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/bbdb/lisp"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/emms/lisp"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/gnuplot"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/doremi"))
+(add-to-list 'load-path (expand-file-name  "~/.emacs.d/packages/undo-tree"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/emacs-calfw"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/wget"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/completion-ui"))
 ;; (add-to-list 'load-path
 ;;              (expand-file-name "~/.emacs.d/packages/org-mode/lisp"))
 ;; (add-to-list 'load-path
 ;;              (expand-file-name "~/.emacs.d/packages/org-mode/contrib/lisp"))
+;;(add-to-list 'load-path
+;;             (expand-file-name "~/EuLisp/EuLisp/emacs"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/emacs-calfw"))
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/packages/w3m"))
 (add-to-list 'load-path
              (expand-file-name "~/.emacs.d/packages/wanderlust/wl"))
 (add-to-list 'load-path
@@ -42,18 +54,6 @@
              (expand-file-name "~/.emacs.d/packages/apel"))
 (add-to-list 'load-path
              (expand-file-name "~/.emacs.d/packages/bbdb"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/w3m"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/yasnippet"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/doremi"))
-;;(add-to-list 'load-path
-;;             (expand-file-name "~/EuLisp/EuLisp/emacs"))
-(add-to-list 'load-path (expand-file-name  "~/.emacs.d/packages/undo-tree"))
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/packages/wget"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/emacs-calfw"))
 (add-to-list 'load-path
              (expand-file-name "~/.emacs.d/packages/w3m"))
 (setq inhibit-startup-message t)  ;; Include in init-eemacs
