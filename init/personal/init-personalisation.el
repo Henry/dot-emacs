a1b739d83859204e1f9fc53f8677747fc4db8d21
Henry Weller <http://cfd.direct>
2016-10-29 20:44:11 +0100

    Separated specialized configurations into another repository

--- init/personal/init-personalisation.el
+++
@@ -1,9 +0,0 @@
-;;; init-personalisation.el --- General personal settings
-;; -----------------------------------------------------------------------------
-;;; Name and Email configuration
-
-(setq user-mail-address "h.weller@cfd.direct"
-      user-full-name "Henry Weller")
-
-;; -----------------------------------------------------------------------------
-;;; init-personalisation.el  ends here.
