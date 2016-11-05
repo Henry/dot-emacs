ef12db97b75e40e97c4bc45fb20d226474e2fd50
Henry Weller <http://cfd.direct>
2016-11-05 12:24:08 +0000

    init-e2wm: Updated for use-package

--- init/init-e2wm.el
+++ init/init-e2wm.el
@@ -1,15 +1,14 @@
 ;;; init-e2wm.el --- Initialize haskell-mode
 ;; -----------------------------------------------------------------------------
 (use-package e2wm
-  :ensure window-layout)
-
-(autoload 'e2wm:start-management "e2wm-code2" "e2wm-code2" t)
-
-(global-set-key (kbd "M-+")
+  :ensure window-layout
+  :init
+  (setq tree-widget-image-enable t)
+  (require 'e2wm-code2)
+  (global-set-key (kbd "M-+")
                 '(lambda ()
                    (interactive)
-                   (require 'e2wm-code2)
-                   (e2wm:start-management '(code2))))
+                   (e2wm:start-management '(code2)))))
 
 ;; -----------------------------------------------------------------------------
 ;;; init-e2wm.el ends here
