839173f8de67b2d9f4f73a17968207fcbbed996d
Henry Weller <http://cfd.direct>
2016-11-25 20:50:15 +0000

    Attempt to sort out windows after commit

--- my-lisp/e2wm-code2.el
+++ my-lisp/e2wm-code2.el
@@ -170,10 +170,10 @@
 
 (setq e2wm:c-document-buffer-p (lambda (buf) nil))
 
-(defvar e2wm:c-code2-show-left-regexp "\\*\\(magit\\)")
+(defvar e2wm:c-code2-show-left-regexp nil)
 
 (defvar e2wm:c-code2-show-right-regexp
-   "\\(\\*\\(Help\\|eshell\\|grep\\|Occur\\|Greed\\|Compilation\\|Backtrace\\|imenu-tree\\|Man\\|WoMan\\|info\\)\\)\\|COMMIT_EDITMSG")
+   "\\(\\*\\(Help\\|eshell\\|grep\\|Occur\\|Greed\\|Compilation\\|Backtrace\\|imenu-tree\\|Man\\|WoMan\\|info\\|magit\\)\\)\\|COMMIT_EDITMSG")
 
 (defvar e2wm:c-code2-max-sub-size 1000)
 
