c18f4935d1195b72fe60fd3e26e2329362491d8a
Henry Weller <http://cfd.direct>
2016-12-08 23:03:57 +0000

    Removed the background colour from elisp buffers

--- init/init-elisp.el
+++ init/init-elisp.el
@@ -174,13 +174,13 @@ otherwise Display the full documentation of FUNCTION (a symbol)."
 ;; -----------------------------------------------------------------------------
 ;;; Set elisp mode background colour
 
-(defface elisp-buffer '((t (:background "azure1")))
-  "*Face used for elisp buffers"
-  :group 'lisp)
+;; (defface elisp-buffer '((t (:background "azure1")))
+;;   "*Face used for elisp buffers"
+;;   :group 'lisp)
 
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (face-remap-add-relative 'default 'elisp-buffer)))
+;; (add-hook 'emacs-lisp-mode-hook
+;;           (lambda ()
+;;             (face-remap-add-relative 'default 'elisp-buffer)))
 
 ;; -----------------------------------------------------------------------------
 ;;; init-elisp.el ends here
