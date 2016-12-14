228726e8dae36af74cd82b2ea3dae3207ababae4
Henry Weller <http://cfd.direct>
2016-12-14 15:39:11 +0000

    Added direct bindings to 'code2' and 'magit' perspectives.

--- init/init-e2wm.el
+++ init/init-e2wm.el
@@ -9,12 +9,26 @@
         tree-widget-image-enable t
         e2wm:prefix-key "C-z e")
   (which-key-add-key-based-replacements e2wm:prefix-key "e2wm")
+
   (require 'e2wm-code2)
   (require 'my-e2wm-vcs)
+
   (defun e2wm-code2 ()
     (interactive)
-    (e2wm:start-management '(code2)))
-  :bind ("M-+" . e2wm-code2)
+    (if e2wm:pst-minor-mode
+        (e2wm:dp-code2)
+      (e2wm:start-management '(code2))))
+
+  (defun e2wm-magit ()
+    (interactive)
+    (if e2wm:pst-minor-mode
+        (e2wm:dp-magit)
+      (e2wm:start-management '(magit))))
+
+  :bind
+  ("C-Z e c" . e2wm-code2)
+  ("C-Z e v" . e2wm-magit)
+
   :config
   (setq e2wm:debug nil))
 
